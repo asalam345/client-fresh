4efd8acf8dff31e845387b82d93d421459e55274
A. B. M. Abdus Salam <asalam345@gmail.com>
2025-03-15 16:02:41 +0600

    Fresh upload partial

---
+++ Fresh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fresh.Startup))]
+namespace Fresh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
